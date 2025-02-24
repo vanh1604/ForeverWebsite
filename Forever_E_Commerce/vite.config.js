f926375aaec5462218281325dee167dc257402d3
vanh1604 <VietAnhScout@gmail.com>
2025-02-24 23:17:43 +0700

    quick fix vercel

--- Forever_E_Commerce/vite.config.js
+++ Forever_E_Commerce/vite.config.js
@@ -5,11 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{port:5174},
-  build: {
-    rollupOptions: {
-      external: [
-        /^node:.*/,
-      ]
-    }
-  }
+ 
 })
