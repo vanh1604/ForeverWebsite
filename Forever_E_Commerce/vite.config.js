7fe8b84a6b54a2ceea9bee516213d90e1d5caf42
vanh1604 <VietAnhScout@gmail.com>
2025-02-25 10:33:01 +0700

    c

--- Forever_E_Commerce/vite.config.js
+++ Forever_E_Commerce/vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:5174},
- 
-})
+  server: { port: 5174 },
+  build: {
+    rollupOptions: {
+      external: ["react-router-dom"],
+    },
+  },
+});
